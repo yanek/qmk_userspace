a8a969f849c342eb9ab3e59cf9c5fae08ed049ef
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-09 17:40:14 +0200

    reformat keymap

--- keyboards/silakka54/keymaps/custom/keymap.c
+++ keyboards/silakka54/keymaps/custom/keymap.c
@@ -38,41 +38,41 @@
 
 enum {
     BSE,
-    SYM,
-    NAV
+    NAV,
+    SYM
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BSE] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,
-        CW_TOGG, HR_A,    HR_S,    HR_D,    HR_F,    KC_G,                               KC_H,    HR_J,    HR_K,    HR_L,    HR_SCLN, KC_QUOT,
-        KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
-                                            KC_NO, MO(NAV), KC_SPC,             KC_ENT,  MO(SYM), KC_RALT
-    ),
-
-    [SYM] = LAYOUT(
-        KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-        KC_NO,   KC_GRV,  EU_LABK, EU_RABK, KC_MINUS, EU_PIPE,                          EU_DCIR, EU_LCBR, EU_RCBR, EU_DLR,  EU_DQUO, KC_NO,
-        KC_NO,   EU_EXLM, KC_ASTR, KC_SLSH, KC_EQUAL, EU_AMPR,                          EU_HASH, EU_LPRN, EU_RPRN, KC_SCLN, EU_QUES, EU_BSLS,
-        KC_NO,   EU_TILD, KC_PLUS, KC_LBRC, KC_RBRC,  EU_PERC,                          EU_AT,   EU_COLN, KC_COMM, KC_DOT,  KC_QUOT, KC_NO,
-                                            KC_TRNS, KC_TRNS, EU_UNDS,          KC_TRNS, KC_TRNS, KC_TRNS
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
+        CW_TOGG, HR_A,    HR_S,    HR_D,    HR_F,    KC_G,           KC_H,    HR_J,    HR_K,    HR_L,    HR_SCLN, KC_QUOT,
+        XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
+                                 XXXXXXX, MO(NAV), KC_SPC,           KC_ENT, MO(SYM), KC_RALT
     ),
 
     [NAV] = LAYOUT(
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_PSCR,                            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-        KC_NO,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_NO,                              KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,   KC_NO,
-        KC_NO,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_TRNS, KC_TRNS, KC_SPC,          KC_ENT, KC_TRNS, KC_TRNS
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_PSCR,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,        KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                   _______, _______, _______,        _______, _______, _______
+    ),
+
+    [SYM] = LAYOUT(
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        XXXXXXX, KC_GRV,  EU_LABK, EU_RABK, KC_MINS,  EU_PIPE,       EU_DCIR, EU_LCBR, EU_RCBR, EU_DLR,  EU_DQUO, XXXXXXX,
+        XXXXXXX, EU_EXLM, KC_ASTR, KC_SLSH, KC_EQL,   EU_AMPR,       EU_HASH, EU_LPRN, EU_RPRN, KC_SCLN, EU_QUES, EU_BSLS,
+        XXXXXXX, EU_TILD, KC_PLUS, KC_LBRC, KC_RBRC,  EU_PERC,       EU_AT,   EU_COLN, KC_COMM, KC_DOT,  KC_QUOT, XXXXXXX,
+                                   _______,  _______, EU_UNDS,       _______, _______, _______
     ),
 };
 
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
     LAYOUT(
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-                       'L', 'L', 'L',  'R', 'R', 'R'
+        'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 
+        'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 
+        'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+                       'L', 'L', 'L', 'R', 'R', 'R'
     );
